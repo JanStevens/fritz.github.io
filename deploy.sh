f086eada7fbd5b236a11829a067b07baddf5cc04
Jan Stevens <jan@playpass.be>
2019-07-11 13:58:40 +0200

    remove deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
-git push origin gh-pages
